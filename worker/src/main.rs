7334c2032e355184932fcbaff8ddedd88f37a94b
Bofulo <newtonombese1@gmail.com>
2026-09-14 13:17:25 +0300

    Setting up the rust program for testing

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("Failed to read input");
+
+    println!("Rust received: {}", input);
 }
