c62caad325fe846ec068b14dd6db74af152ac876
Bofulo <newtonombese1@gmail.com>
2026-09-14 23:41:07 +0300

    Working on the json version of data that will be accessed by the two stacks

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -1,5 +1,11 @@
 use std::io::{self, Read};
+use serde::Deserialize;
 
+
+#[derive(Deserialize)]
+struct Input {
+    numbers: Vec<i32>,
+}
 fn main() {
     let mut input = String::new();
 
@@ -8,4 +14,9 @@ fn main() {
         .expect("Failed to read input");
 
     println!("Rust received: {}", input);
+
+    let data: Input = serde_json::from_str(&input)
+        .expect("Failed to parse json");
+
+    println!("{:?}", data.numbers);
 }
