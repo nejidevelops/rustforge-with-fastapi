feab31b4811718f5c8b198c14cec76c527553cf4
Bofulo <newtonombese1@gmail.com>
2026-09-19 00:54:25 +0300

    Working on thFASTAPI side executing rust

--- api/main.py
+++ api/main.py
@@ -2,9 +2,16 @@ import subprocess
 
 result = subprocess.run(
   ["../worker/target/debug/worker.exe"],
-  input="Hello from Python",
+  input='{"numbers":[10,20,30,40]}',
   text=True,
   capture_output=True,
 )
 
+print("Rust stdout:")
 print(result.stdout)
+
+print("Rust stderr:")
+print(result.stderr)
+
+print("Rust exit code:")
+print(result.returncode)  
