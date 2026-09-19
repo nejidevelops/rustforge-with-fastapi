b44150e737f572a0ae4fa3a6a11c9fb83df863ac
Bofulo <newtonombese1@gmail.com>
2026-09-19 09:50:34 +0300

    Workingo n the source /c/Users/User/Desktop/learning/rustforge/.venv/Scripts/activate

--- api/main.py
+++ api/main.py
@@ -1,31 +1,35 @@
 import subprocess
 import json
 
-payload = {
-  "numbers": [10,20,30,40]
-}
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-json_input = json.dumps(payload)
+app = FastAPI()
 
-print(json_input)
-result = subprocess.run(
+class CalculationRequest(BaseModel):
+  numbers: list[int]
+
+@app.get("/")
+def root():
+  return {"message":"Hello from Rust Forge"}
+
+@app.post("/calculate")
+def calculate(request: CalculationRequest):
+  payload = {
+  "numbers": request.numbers
+  }
+
+  json_input = json.dumps(payload)
+
+  print(json_input)
+
+  result = subprocess.run(
   ["../worker/target/debug/worker.exe"],
   input=json_input,
   text=True,
   capture_output=True,
-)
+  )
 
-print("Rust stdout:")
-print(result.stdout)
-
-print("Rust stderr:")
-print(result.stderr)
-
-print("Rust exit code:")
-print(result.returncode)
-
-if result.returncode == 0:
   output = json.loads(result.stdout)
 
-  print("Parsed Result:")
-  print(output)
+  return output
