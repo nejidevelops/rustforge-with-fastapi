900da02544ed498ccf7c454f06a9e714e9c54df9
Bofulo <newtonombese1@gmail.com>
2026-09-19 09:25:17 +0300

    Working on the python script

--- api/main.py
+++ api/main.py
@@ -1,8 +1,16 @@
 import subprocess
+import json
 
+payload = {
+  "numbers": [10,20,30,40]
+}
+
+json_input = json.dumps(payload)
+
+print(json_input)
 result = subprocess.run(
   ["../worker/target/debug/worker.exe"],
-  input='{"numbers":[10,20,30,40]}',
+  input=json_input,
   text=True,
   capture_output=True,
 )
@@ -14,4 +22,10 @@ print("Rust stderr:")
 print(result.stderr)
 
 print("Rust exit code:")
-print(result.returncode)  
+print(result.returncode)
+
+if result.returncode == 0:
+  output = json.loads(result.stdout)
+
+  print("Parsed Result:")
+  print(output)
