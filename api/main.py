5fa093694946956d7425623ae5ef59b3e3388049
Bofulo <newtonombese1@gmail.com>
2026-09-24 08:31:02 +0300

    Working on the upgrades and upgrades

--- api/main.py
+++ api/main.py
@@ -1,18 +1,15 @@
-import subprocess
-import json
-
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 from config import RUST_WORKER
-from worker import RustWorker
+from worker import WorkerPool
 
 app = FastAPI()
 
 class CalculationRequest(BaseModel):
   numbers: list[int]
 
-rust_worker = RustWorker(RUST_WORKER)
+rust_worker = WorkerPool(RUST_WORKER, size=4)
 
 @app.get("/")
 def root():
